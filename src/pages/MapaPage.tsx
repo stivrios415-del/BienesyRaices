92e3db19f8ea9dc04ce0f93dc0df152f296d4c37
Ronald Matute <stivrios415@gmail.com>
2026-08-11 06:30:07 -0600

    Refactor MapaPage to use loteSeleccionadoId

--- src/pages/MapaPage.tsx
+++ src/pages/MapaPage.tsx
@@ -4,12 +4,12 @@ import LoteDetallePanel from '../components/LoteDetallePanel'
 import type { Lote } from '../types/lote'
 
 export default function MapaPage() {
-  const [loteSeleccionado, setLoteSeleccionado] = useState<Lote | null>(null)
+  const [loteSeleccionadoId, setLoteSeleccionadoId] = useState<string | null>(null)
 
   return (
     <div className="flex-1 relative overflow-hidden">
-      <MapaLotes onSelect={setLoteSeleccionado} />
-      <LoteDetallePanel lote={loteSeleccionado} onClose={() => setLoteSeleccionado(null)} />
+      <MapaLotes onSelect={(lote: Lote) => setLoteSeleccionadoId(lote.id)} />
+      <LoteDetallePanel loteId={loteSeleccionadoId} onClose={() => setLoteSeleccionadoId(null)} />
     </div>
   )
 }
