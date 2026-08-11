39f704949d0d3f5cb8c4b70c17cc081585ada719
Ronald Matute <stivrios415@gmail.com>
2026-08-11 06:30:23 -0600

    Refactor TablaPage to use loteSeleccionadoId

--- src/pages/TablaPage.tsx
+++ src/pages/TablaPage.tsx
@@ -4,12 +4,12 @@ import LoteDetallePanel from '../components/LoteDetallePanel'
 import type { Lote } from '../types/lote'
 
 export default function TablaPage() {
-  const [loteSeleccionado, setLoteSeleccionado] = useState<Lote | null>(null)
+  const [loteSeleccionadoId, setLoteSeleccionadoId] = useState<string | null>(null)
 
   return (
     <div className="flex-1 overflow-y-auto">
-      <TablaLotes onSelect={setLoteSeleccionado} />
-      <LoteDetallePanel lote={loteSeleccionado} onClose={() => setLoteSeleccionado(null)} />
+      <TablaLotes onSelect={(lote: Lote) => setLoteSeleccionadoId(lote.id)} />
+      <LoteDetallePanel loteId={loteSeleccionadoId} onClose={() => setLoteSeleccionadoId(null)} />
     </div>
   )
 }
